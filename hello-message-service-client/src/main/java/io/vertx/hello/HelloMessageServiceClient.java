f79440b407413acc715c3c2eb63588ea94ab0dfb
vision8 tech <vision8r@gmail.com>
2017-07-03 11:54:14 +0300

    client resilience included

--- hello-message-service-client/src/main/java/io/vertx/hello/HelloMessageServiceClient.java
+++ hello-message-service-client/src/main/java/io/vertx/hello/HelloMessageServiceClient.java
@@ -2,9 +2,12 @@ package io.vertx.hello;
 
 import io.vertx.core.json.JsonObject;
 import io.vertx.rxjava.core.AbstractVerticle;
+import io.vertx.rxjava.core.RxHelper;
 import io.vertx.rxjava.core.eventbus.EventBus;
 import io.vertx.rxjava.core.eventbus.Message;
 import rx.Single;
+
+import java.util.concurrent.TimeUnit;
 
 public class HelloMessageServiceClient extends AbstractVerticle {
 	
@@ -15,8 +18,18 @@ public class HelloMessageServiceClient extends AbstractVerticle {
 				.requestHandler(req -> {
 					EventBus bus = vertx.eventBus();
 					Single<JsonObject> lukeReply = bus.<JsonObject>rxSend("hello", "Luke")
+							// timeout treatment
+							.subscribeOn(RxHelper.scheduler(vertx))
+							.timeout(3, TimeUnit.SECONDS)
+							.retry()
+							// finally, getting the message body
 							.map(Message::body);
 					Single<JsonObject> leiaReply = bus.<JsonObject>rxSend("hello", "Leia")
+							// timeout treatment
+							.subscribeOn(RxHelper.scheduler(vertx))
+							.timeout(3, TimeUnit.SECONDS)
+							.retry()
+							// finally, getting the message body
 							.map(Message::body);
 					
 					Single.zip(lukeReply, leiaReply, (luke, leia) ->
@@ -31,7 +44,7 @@ public class HelloMessageServiceClient extends AbstractVerticle {
 									result -> req.response().end(result.encodePrettily()),
 									error -> req.response()
 											.setStatusCode(500)
-											.end(error.getMessage()));
+											.end(new JsonObject().put("error", error.getMessage()).encode()));
 				})
 				.listen(8082);
 	}
