d1b047b30839cabf72427abb6b2264520eb78454
vision8 tech <vision8r@gmail.com>
2017-10-11 23:13:33 +0300

    step 1 beginnings

--- vertx-wiki/src/main/java/io/vertx/guides/wiki/MainVerticle.java
+++ vertx-wiki/src/main/java/io/vertx/guides/wiki/MainVerticle.java
@@ -1,14 +1,24 @@
-package io.vertx.starter;
+package io.vertx.guides.wiki;
 
 import io.vertx.core.AbstractVerticle;
+import io.vertx.core.Future;
 
 public class MainVerticle extends AbstractVerticle {
 
+  
   @Override
   public void start() {
+    
     vertx.createHttpServer()
         .requestHandler(req -> req.response().end("Hello Vert.x!"))
         .listen(8080);
+  }
+  
+  
+  @Override
+  public void start(Future<Void> startFuture) throws Exception {
+    
+    startFuture.complete();
   }
 
 }
