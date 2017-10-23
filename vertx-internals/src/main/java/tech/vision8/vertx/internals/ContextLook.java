62c1a49fe9ef2f4c4ee6d7f6107be3169d20f74e
vision8 tech <vision8r@gmail.com>
2017-10-23 22:56:39 +0300

    using event loop pool size

--- vertx-internals/src/main/java/tech/vision8/vertx/internals/ContextLook.java
+++ vertx-internals/src/main/java/tech/vision8/vertx/internals/ContextLook.java
@@ -1,6 +1,7 @@
 package tech.vision8.vertx.internals;
 
 import io.vertx.core.Vertx;
+import io.vertx.core.VertxOptions;
 
 /**
  * @author vision8
@@ -10,7 +11,16 @@ public class ContextLook {
 	public static void main(String[] args) {
 		
 		System.out.println(Thread.currentThread());
-		Vertx vertx = Vertx.vertx();
+		
+		// By default, the number of event loop threads are twice the number of CPU cores.
+		// Example:
+		//   * On my 8-core MBP, 16 event loop threads are created.
+		//   * In this case, output shows "vert.x-eventloop-thread-0" to "vert.x-eventloop-thread-15".
+		// Vertx vertx = Vertx.vertx();
+		
+		// However, a custom value can be provided:
+		Vertx vertx = Vertx.vertx(new VertxOptions().setEventLoopPoolSize(10));
+		
 		for (int i = 0; i < 20; i++) {
 			int index = i;
 			vertx.setTimer(1, timerID -> {
